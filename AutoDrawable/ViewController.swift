bb45acddfaf3ae83c0bd5b53d53891004b2cfb78
Carlos <toxz@foxmail.com>
2015-12-09 09:52:10 +0800

    Init commit

---
+++ AutoDrawable/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AutoDrawable
+//
+//  Created by ParkerCarlos on 12/9/15.
+//  Copyright (c) 2015 Unique Studio. All rights reserved.
+//
+
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    }
+
+
+    override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+    }
+
+
+
+
+}
---
+++ Doc/RoadMap.md
@@ -0,0 +1,7 @@
+# RoadMap
+
+## v0.0.1
+
+- monitor project folder changes
+- auto move drawable file to latest created folder
+- change current work folder
